81293ccfdecd3f97371eb8afb460adaa467bd5ab
amakk3 <amakk3@uic.edu>
2025-10-15 16:31:01 -0500

    cors headers, react server port at 3000

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,11 +40,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third‑party apps
+    'rest_framework',        # Django REST Framework
+    'corsheaders',           # For handling CORS (React <-> Django)
+
     # Our apps
     'civicapi',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # Must be near the top
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,3 +130,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # React dev server
+]
