27a083000afaa1f364239fa25d77f1343b9591fc
Thuy Duong Pham <132012852+tpham66@users.noreply.github.com>
2025-11-11 14:10:10 -0600

    .

--- backend/civicapi/views.py
+++ backend/civicapi/views.py
@@ -6,7 +6,7 @@ from .trie import Trie
 from django.views.decorators.csrf import csrf_exempt
 from .utils import log_query
 from utils.bloom_filter import BloomFilter
-from .models import Candidate
+from .models import Candidate, User
 from datetime import datetime
 
 # Create your views here.
